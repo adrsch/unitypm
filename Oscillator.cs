333dfad000a3fdfbeabf6373dd517b7d8010b62a
adrsch <35346279+adrsch@users.noreply.github.com>
2018-11-17 23:32:15 -0800

    Added voicing, finished envelopes

--- Oscillator.cs
+++ Oscillator.cs
@@ -3,49 +3,147 @@ using System.Collections.Generic;
 using UnityEngine;
 using System;
 
-namespace Oscillator
+public class Oscillator : MonoBehaviour
 {
-    public class Oscillator : MonoBehaviour
-    {
-        private float frequency; //Set as needed by PMSynth component.
-        public float ratio;
-        public float amplitude; //Range is 0 to 1. Use 0 to -1 for inversion. Values beyond 1 will cause distortion.
-        public float offset; //Shifts the phase of the waves.
-        public float feedback; //Range is 0 to 1. Uses the previous calculation's data as if it were a standard modulator. This number functions as the modulation index would.
+    //Oscillator Properties
+    private float[] frequency; //Set as needed by PMSynth component.
+    private byte[] velocity; //Set as needed by PMSynth component.
+    private bool[] voiceOn;
+    private byte voices;
 
-        private float data = 0;
-        private float sampleRate;
-        private ulong time = 0; //Will cause a click once this wraps around. Thankfully, this will not happen often, but it could be an issue for a continuously playing tone.
-        public Oscillator[] modulators = new Oscillator[0];
-        public float[] modulationIndicies = new float[0]; 
+    private bool[] playing;
+
+    public float ratio = 1;
+    public float amplitude = 1; //Range is 0 to 1. Use 0 to -1 for inversion. Values beyond 1 will cause distortion.
+    public float offset = 0; //Shifts the phase of the waves.
+    public float feedback = 0; //Range is 0 to 1. Uses the previous calculation's data as if it were a standard modulator. This number functions as the modulation index would.
+
+    //Modulators
+    private float data = 0;
+    private float sampleRate;
+    private ulong[] time; //Will cause a click once this wraps around. Thankfully, this will not happen often, but it could be an issue for a continuously playing tone.
+    public Oscillator[] modulators = new Oscillator[0]; //Please don't start an infinite loop of modulators. This will cause a stack overflow.
+    public float[] modulationIndicies = new float[0]; //Optional. Acts as multiplier to the data of the corresponding modulator. Will default to 1.
+
+    //Envelope
+    public float attack;
+    public float decay;
+    public float sustain;
+    public float release;
+    
+    private bool[] onRelease;
+    private float[] offTime; //Used in calculating data during release
+    private float[] offEnvelopeScalar; //Used in calculating data during release
+
+    //Used for when osc a is modulated by osc b which is modulated by a. 
+    private bool recursion = false;
         
-        public void setSampleRate(float sampleRate)
-        {
-            this.sampleRate = sampleRate;
-            for (int i = 0; i < modulators.Length; i++)
+    public void SetSampleRate(float sampleRate)
+    {
+        this.sampleRate = sampleRate;
+        for (int i = 0; i < modulators.Length; i++)
+            if (modulators[i] != null)
                 modulators[i].sampleRate = sampleRate;
-        }
-
-        public void setFrequency(float frequency)
-        {
-            this.frequency = frequency;
-            for (int i = 0; i < modulators.Length; i++)
-                if (modulators[i] != null)
-                    modulators[i].setFrequency(frequency);
-        }
-
-        public float generateData()
-        {
-            float phaseModulation = 0;
-            for (int i = 0; i < modulators.Length; i++)
-                if (modulators[i] != null)
-                    if (i < modulationIndicies.Length)
-                        phaseModulation += modulationIndicies[i] * modulators[i].generateData();
-                    else
-                        phaseModulation += modulators[i].generateData();
-            data = amplitude * Mathf.Sin((2 * Mathf.PI * ratio * frequency * time) / sampleRate + 1f * phaseModulation + feedback*data + offset); //Note that the feedback has a delay on it.
-            time++;
-            return data;
-        }
     }
+
+    public void SetVoices(byte voices)
+    {
+        if (recursion)
+            return;
+        recursion = true;
+        frequency = new float[voices];
+        velocity = new byte[voices];
+        voiceOn = new bool[voices];
+        onRelease = new bool[voices];
+        playing = new bool[voices];
+        time = new ulong[voices];
+        offTime = new float[voices];
+        offEnvelopeScalar = new float[voices];
+        this.voices = voices;
+        for (int i = 0; i < modulators.Length; i++)
+            if (modulators[i] != null)
+                modulators[i].SetVoices(voices);
+        recursion = false;
+    }
+
+
+    public float GenerateData(byte voice)
+    {
+        if (playing[voice] == false)
+            return float.NegativeInfinity; //This will set the synth to set the voice as inactive
+        if (recursion)
+            return data; //Feeding an oscillator's modulator the oscillator itself will result in a delay as is the case with feedback. 
+        recursion = true;
+        float phaseModulation = 0;
+        for (int i = 0; i < modulators.Length; i++)
+            if (modulators[i] != null)
+                if (i < modulationIndicies.Length)
+                    phaseModulation += modulationIndicies[i] * modulators[i].GenerateData(voice);
+                else
+                    phaseModulation += modulators[i].GenerateData(voice);
+        data = GenerateEnvelopeScalar(voice) * amplitude * Mathf.Sin((float) (4 * Mathf.PI * ratio * frequency[voice] * time[voice]) / sampleRate + 1f * phaseModulation + feedback*data + offset); //Note that the feedback has a delay on it.
+        //data = GenerateEnvelopeScalar(voice) * amplitude * Mathf.Sin((float)(4 * Mathf.PI * ratio * frequency[voice] * time[voice] * AudioSettings.dspTime) / sampleRate + 1f * phaseModulation + feedback * data + offset); //Fun sounding version, for testing purposes only.
+        time[voice]++;
+        recursion = false;
+        return data;
+    }
+
+    public float GenerateEnvelopeScalar(byte voice)
+    {
+        float seconds = time[voice] / sampleRate;
+        if (onRelease[voice] == false)
+        {
+            seconds = time[voice] / sampleRate;
+            if ( seconds - attack < 0)
+                return seconds / attack;
+            if ((seconds - attack) - decay < 0)
+                return 1 - (1- sustain) * (seconds - attack) / decay;
+            return sustain;
+        }
+        if (seconds - offTime[voice] < release)
+            return offEnvelopeScalar[voice] * (1 -  (seconds - offTime[voice]) / release);
+        playing[voice] = false;
+        return 0;
+    }
+
+    public int NoteOn(float frequency, byte velocity)
+    {
+        recursion = true;
+        for (byte i = 0; i < voices; i++)
+            if (voiceOn[i] == false)
+            {
+                voiceOn[i] = true;
+                this.frequency[i] = frequency;
+                time[i] = 0;
+                playing[i] = true;
+                for (int j = 0; j < modulators.Length; j++)
+                    if (modulators[j] != null)
+                        modulators[j].NoteOn(frequency, velocity, i);
+                recursion = false;
+                return (int)i;
+            }
+        return 256; //as this is larger than the highest possible voice, it is used as a failure to find a new voice
+    }
+
+    public void NoteOn(float frequency, byte velocity, byte voice)
+    {
+        if (recursion)
+            return; 
+        recursion = true;
+        playing[voice] = true;
+        time[voice] = 0;
+        this.frequency[voice] = frequency;
+        for (int i = 0; i < modulators.Length; i++)
+            if (modulators[i] != null)
+                modulators[i].NoteOn(frequency, velocity, voice);
+        recursion = false;
+    }
+
+    public void NoteOff(byte voice)
+    {
+        offTime[voice] = time[voice] / sampleRate;
+        offEnvelopeScalar[voice] = GenerateEnvelopeScalar(voice);
+        onRelease[voice] = true;
+    }
+
 }
--- PMSynth.cs
+++ PMSynth.cs
@@ -6,33 +6,89 @@ public class PMSynth : MonoBehaviour
 {
     public float frequency;
     public float amplitude;
+    public byte voices;
     private float sampleRate;
-    public Oscillator.Oscillator[] outputOscillators = new Oscillator.Oscillator[0];
+    public Oscillator[] outputOscillators = new Oscillator[0];
+    private bool[] activeVoices;
 
     void Start()
     {
+        activeVoices = new bool[voices];
         sampleRate = AudioSettings.outputSampleRate;
         for (int i = 0; i < outputOscillators.Length; i++)
         {
-            outputOscillators[i].setFrequency(frequency);
-            outputOscillators[i].setSampleRate(sampleRate);
+            outputOscillators[i].SetSampleRate(sampleRate);
+            outputOscillators[i].SetVoices(voices);
         }
+
+        Application.targetFrameRate = 10;
     }
 
+    void NoteOn(float frequency, byte velocity)
+    {
+
+    }
+
+    void NoteOn(float frequency)
+    {
+
+    }
+
+    void NoteOn(byte pitch, byte velocity)
+    {
+
+    }
+
+    void NoteOff()
+    {
+
+    }
+
+    private int t = 0;
     void OnAudioFilterRead(float[] data, int channels)
     {
+         if (t > 100000000)
+            t = 0;
+
         for (int n = 0; n < data.Length / channels; n++)
         {
             //Sum up the outputs from all oscillators.
             float oscOutputs = 0;
             for (int j = 0; j < outputOscillators.Length; j++)
-                oscOutputs += outputOscillators[j].generateData();
+            {
+                if (t == 0)
+                {
+                    int voiceUsed = outputOscillators[j].NoteOn(frequency, 255);
+                    if (voiceUsed != 256)
+                    {
+                        activeVoices[voiceUsed] = true;
+                    }
+                }
+                if (t == 100000)
+                {
+                    for (byte i = 0; i < voices; i++)
+                        if (activeVoices[i] == true)
+                            outputOscillators[i].NoteOff(i);
 
+                }
+                for (byte i = 0; i < voices; i++)
+                    if (activeVoices[i] == true)
+                    {
+                        float oscData = outputOscillators[j].GenerateData(i);
+                        if (float.IsNegativeInfinity(oscData))
+                            activeVoices[i] = false;
+                        else
+                            oscOutputs += oscData;
+                    }
+            }
             //Currently, no stereo support.
             for (int i = 0; i < channels; i++)
                 data[n * channels + i] = amplitude * oscOutputs;
 
             n++;
+
+
+            t++;
         }
     }
 }
